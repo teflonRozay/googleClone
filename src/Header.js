73c64354735e1824bd092c04725acff914821e74
teflonRozay <jigaj4allnation@gmail.com>
2024-05-02 18:06:12 +0100

    'third commit'

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 // Header.js
 import React from 'react';
+import menuIcon from './src/images/menuIcon.png'
 
 function Header() {
     return (
@@ -12,7 +13,7 @@ function Header() {
                     <a href="google.com/images">Images</a>
                 </li>
                 <li>
-                    <a href="google.com/icon"><img src="./images/menuIcon.png" alt="images" /></a>
+                    <a href="google.com/icon"><img src={menuIcon} alt="images" /></a>
                 </li>
                 <li>
                     <button>Sign In</button>
